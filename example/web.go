24e1820a6717f39423851ee5ecf6e34b9479e56d
tongtao.ztt <tongtao.ztt@alibaba-inc.com>
2022-03-01 13:37:09 +0800

    d

--- example/web.go
+++ example/web.go
@@ -3,8 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-
-	_ "github.com/CodyGuo/godaemon"
 )
 
 func main() {
--- godaemon.go
+++ godaemon.go
@@ -8,16 +8,11 @@ import (
 )
 
 func init() {
-	goDaemon := flag.Bool("d", false, "run app as a daemon with -d=true.")
-	flag.Parse()
-
-	if *goDaemon {
-		cmd := exec.Command(os.Args[0], flag.Args()...)
-		if err := cmd.Start(); err != nil {
-			fmt.Printf("start %s failed, error: %v\n", os.Args[0], err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid)
-		os.Exit(0)
+	cmd := exec.Command(os.Args[0], flag.Args()...)
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("start %s failed, error: %v\n", os.Args[0], err)
+		os.Exit(1)
 	}
+	fmt.Printf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid)
+	os.Exit(0)
 }
